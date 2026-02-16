8db156d8dd3407094c1abbc18014021052f0a71a
hrithiksuresh852@gmail.com <hrithiksuresh852@gmail.com>
2026-02-17 02:06:54 +0530

    feat: add cookie-parser for handling cookies and enhance post creation with JWT validation

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,19 +1,45 @@
-const express=require('express')
+const postModel=require('../models/post.model');
 const ImageKit=require('@imagekit/nodejs')
-const { toFile }=require('@imagekit/nodejs')
+const {toFile}=require('@imagekit/nodejs')
+const jwt=require('jsonwebtoken')
 const client=new ImageKit({
-  privateKey: process.env.IMAGE_KIT, // This is the default and can be omitted
+    privateKey:process.env.IMAGE_KIT
 })
-const createPostController=async(req,res)=>{
-    const {caption}=req.body;
- console.log(req.body,req.file);
- const file=await client.files.upload({
-     file: await toFile(Buffer.from(req.file.buffer), 'file'),
-     fileName: 'test',
-    });
-    res.send(file)
+let createPostController=async(req,res)=>{
+    console.log(req.body,req.file)
+
+    const token=req.cookies.token;
+    if(!token){
+        return res.status(401).json({message:'token not found'})
+    }
+    let decoded=null;
+    try{
+        decoded=jwt.verify(token,process.env.JWT_SECRET)
+    }
+    catch(err){
+        return res.status(401).json({
+            message:'user not authorized'
+        })
+    }
+
+
+    const file=await client.files.upload({
+        file:await toFile(Buffer.from(req.file.buffer),'file'),
+        fileName:'postImage'
+    })
+
+    const post=await postModel.create({
+        caption:req.body.caption,
+        ImageUrl:file.url,
+        user:decoded.id,
+
+    })
+    res.status(201).json({
+        message: "Post created successfully.",
+        post
+    })
 }
 
 module.exports={
-    createPostController,
-};
+    createPostController
+}
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,9 +1,8 @@
-const express=require('express')
+const express=require('express');
+const multer=require('multer')
 const postController=require('../controllers/post.controller')
 const postRouter=express.Router();
-const multer=require('multer')
-const storage = multer.memoryStorage()
-const upload = multer({ storage: storage })
+const upload=multer({storage:multer.memoryStorage()})
 postRouter.post('/',upload.single('photo'),postController.createPostController)
 
 module.exports=postRouter;
